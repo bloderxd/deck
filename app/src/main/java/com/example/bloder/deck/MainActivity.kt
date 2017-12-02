f39a4c9a9757a9e721acfa3a95c795a594b881ea
Bloder <aptgetinstalldanielleme@gmail.com>
2017-12-02 17:11:18 -0200

    Config deck pager in main activity

--- app/src/main/java/com/example/bloder/deck/MainActivity.kt
+++ app/src/main/java/com/example/bloder/deck/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.bloder.deck
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.lib.Deck
 
 /**
  * Created by bloder on 25/08/17.
  */
 class MainActivity : AppCompatActivity() {
 
+    private val deckPager by lazy { findViewById<Deck>(R.id.deck_pager) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        deckPager.offscreenPageLimit = 5
+        deckPager.adapter = DeckAdapter(this)
+        deckPager.useDefaultPadding(this)
     }
 }
