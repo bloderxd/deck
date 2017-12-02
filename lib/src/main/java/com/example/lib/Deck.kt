3d0e9073c86b857075d8fd43f5e83824c7cd83cb
Bloder <aptgetinstalldanielleme@gmail.com>
2017-12-02 17:18:19 -0200

    Create default padding config into deck class

--- lib/src/main/java/com/example/lib/Deck.kt
+++ lib/src/main/java/com/example/lib/Deck.kt
@@ -1,8 +1,10 @@
 package com.example.lib
 
+import android.app.Activity
 import android.content.Context
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 
 /**
  * Created by bloder on 25/08/17.
@@ -15,4 +17,13 @@ class Deck : ViewPager {
     init { initView() }
 
     private fun initView() = this.setPageTransformer(true, CoverFlowTransformer())
+
+    fun useDefaultPadding(context: Activity) {
+        val metrics = DisplayMetrics()
+        context.windowManager.defaultDisplay.getMetrics(metrics)
+        val padding = (metrics.widthPixels * 0.08).toInt()
+        setPadding(padding, 0, padding, 0)
+        clipToPadding = false
+        pageMargin = 0
+    }
 }
